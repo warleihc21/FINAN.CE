e185482fba7f0c025cc1ac67c3757f8215c423aa
Warlei <warleihc21@gmail.com>
2023-07-12 21:46:47 -0300

    tentativa de deploy

--- core/settings.py
+++ core/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-1fr!wxs%rhkqsshslm$s&=*&+sfl905fsq!93yqr=@67j!6-hf
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
